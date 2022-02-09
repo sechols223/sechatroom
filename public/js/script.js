0a612f79d25e613d520aa12c6e10eb1d22127530
Samuel Echols <samuel.echols@selu.edu>
2022-02-09 12:49:42 -0600

    db

--- public/js/script.js
+++ public/js/script.js
@@ -17,23 +17,3 @@ $("#copy").click(function() {
         window.getSelection().removeAllRanges();
     }
 )
-const redis = require('redis');
-const client = redis.createClient( {
-    host: 'ec2-54-164-111-86.compute-1.amazonaws.com',
-    port: '12710',
-    password: 'p5917f69b10d2df9e1c3e5188530a3b09dd80aab3f74c60f083b52e1b84ca2cb1'
-});
-
-client.on('error', err => {
-    console.log("Error " + err);
-});
-
-client.set('foo', 'bar', (err, reply) => {
-    if (err) throw err;
-    console.log(reply);
-
-    client.get('foo', (err, reply) => {
-        if (err) throw err;
-        console.log(reply);
-    });
-});
