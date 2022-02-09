753bae11a4225a34da17babf03d550ce5d38073f
Progamer2600 <samuelechols223@gmail.com>
2022-02-09 17:03:25 -0600

    establishing socket connection

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public/db'));
 app.use(express.static('public/socket'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(port, () => {
