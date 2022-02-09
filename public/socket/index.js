64530cf40e77b2497eb9c0bb0af2f76fee184c1c
Progamer2600 <samuelechols223@gmail.com>
2022-02-09 17:10:16 -0600

    establishing socket connection

--- public/socket/index.js
+++ public/socket/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-io.on('connection', (socket => {
-    console.log('user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
     });
-}));
+});
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
