14236e1d21580fe38f257607f4199a03f7269b86
Progamer2600 <samuelechols223@gmail.com>
2022-02-09 16:43:21 -0600

    establishing socket connection

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const redis = require('redis');
-const fs = require('fs');
-const client = redis.createClient({
-    url: 'rediss://:p5917f69b10d2df9e1c3e5188530a3b09dd80aab3f74c60f083b52e1b84ca2cb1@ec2-54-164-111-86.compute-1.amazonaws.com:12710',
-    socket: {
-        tls: true,
-        rejectUnauthorized: false
-    }
-});
 
-client.connect();
-client.on('connect',function() {
-    console.log('Connection Established');
-});
-
-client.on('error', err => {
-    console.log('Error ' + err);
-});
-
-client.set('foo', 'bar', (err, reply) => {
-    if (err) throw err;
-    console.log(reply);
-
-    client.get('foo', (err, reply) => {
-        if (err) throw err;
-        console.log(reply);
-    });
-});
 
 app.use(express.static("public"));
 
--- public/db/index.js
+++ public/db/index.js
@@ -1,2 +1,29 @@
+const redis = require('redis');
+const fs = require('fs');
+const client = redis.createClient({
+    url: 'rediss://:p5917f69b10d2df9e1c3e5188530a3b09dd80aab3f74c60f083b52e1b84ca2cb1@ec2-54-164-111-86.compute-1.amazonaws.com:12710',
+    socket: {
+        tls: true,
+        rejectUnauthorized: false
+    }
+});
 
+client.connect();
+client.on('connect',function() {
+    console.log('Connection Established');
+});
+
+client.on('error', err => {
+    console.log('Error ' + err);
+});
+
+client.set('foo', 'bar', (err, reply) => {
+    if (err) throw err;
+    console.log(reply);
+
+    client.get('foo', (err, reply) => {
+        if (err) throw err;
+        console.log(reply);
+    });
+});
 
