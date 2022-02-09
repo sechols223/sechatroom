bf8e8b44c505e7b747b01fb3cb70db8445ea4795
Progamer2600 <samuelechols223@gmail.com>
2022-02-09 16:54:11 -0600

    establishing socket connection

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static("public"));
-
+app.use(express.static('public/css'));
+app.use(express.static('public/js'));
+console.log('using db');
+app.use(express.static('public/db'));
+app.use(express.static('public/socket'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
