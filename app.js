eccf8c0c7eb1960973e735910216a06026d66820
Progamer2600 <samuelechols223@gmail.com>
2022-02-09 16:35:11 -0600

    db

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const redis = require('redis');
-const client = redis.createClient( {
-    host: 'redis://:p5917f69b10d2df9e1c3e5188530a3b09dd80aab3f74c60f083b52e1b84ca2cb1@ec2-54-164-111-86.compute-1.amazonaws.com:12709',
-    port: '12710',
-    password: 'p5917f69b10d2df9e1c3e5188530a3b09dd80aab3f74c60f083b52e1b84ca2cb1'
+const client = redis.createClient({
+    url: 'redis://:p5917f69b10d2df9e1c3e5188530a3b09dd80aab3f74c60f083b52e1b84ca2cb1@ec2-54-164-111-86.compute-1.amazonaws.com:12709',
+    socket: {
+        tls: true,
+        rejectUnauthorized: false
+    }
 });
 
 client.connect();
