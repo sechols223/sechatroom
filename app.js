c7f53a3aa41438c79f94e304e76878a989e19084
Samuel Echols <samuel.echols@selu.edu>
2022-02-10 15:02:51 -0600

    Implementing socket changes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(express.static('public/css'));
 app.use(express.static('public/js'));
 console.log('using db');
