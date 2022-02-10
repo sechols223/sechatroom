a4b76890eaa3a0d8240db555e13742edc0327d69
Samuel Echols <samuel.echols@selu.edu>
2022-02-10 15:10:32 -0600

    Implementing socket changes

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.use(express.static('public/css'));
 app.use(express.static('public/js'));
 console.log('using db');
@@ -12,6 +17,21 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
