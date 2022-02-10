6b545a9c137354064114f794a0e13ee80b1c74cd
Samuel Echols <samuel.echols@selu.edu>
2022-02-10 16:34:02 -0600

    Implementing socket changes

--- app.js
+++ app.js
@@ -15,21 +15,10 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
-});
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
@@ -37,5 +26,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-    console.log('listening on *:3000')
-})
+    console.log('listening on port: &{port}');
+});
